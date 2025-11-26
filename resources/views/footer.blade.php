ac6dbfd3c5eeb543d1901d00dc176b99cb588e45
DevOps Informática – Municipalidad de Pichilemu <devops.informatica@pichilemu.cl>
2025-11-26 12:43:30 -0300

    footer: Version bump

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="w-full p-4">
     <p class="text-xs text-center text-gray-500 dark:text-gray-400">
-        Desarrollado por la Oficina de Computación e Informática – Municipalidad de Pichilemu – v3.0
+        Desarrollado por la Oficina de Computación e Informática – Municipalidad de Pichilemu – v3.1
     </p>
 </footer>
