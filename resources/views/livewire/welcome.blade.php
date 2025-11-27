81333e5dab330d8edf558752329a1cb5430b16fb
DevOps Informática – Municipalidad de Pichilemu <devops.informatica@pichilemu.cl>
2025-11-27 12:55:32 -0300

    Pint: Reformateado

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -14,8 +14,9 @@ new class extends Component {
 
 <div class="grid grid-cols-1 lg:grid-cols-2 min-h-[80vh] gap-8">
     <div class="lg:grid place-content-center py-8 lg:px-8">
-        <div class="bg-gradient-to-br from-transparent to-gray-300/5 backdrop-blur-xl lg:rounded-lg lg:shadow-lg p-6 text-center">
-            <x-fas-star class="animate-pulse h-24 mx-auto text-yellow-500 mb-6" />
+        <div
+            class="bg-gradient-to-br from-transparent to-gray-300/5 backdrop-blur-xl lg:rounded-lg lg:shadow-lg p-6 text-center">
+            <x-fas-star class="animate-pulse h-24 mx-auto text-yellow-500 mb-6"/>
 
             <h1 class="text-4xl font-bold mb-4">#FelizNavidad</h1>
 
@@ -26,12 +27,14 @@ new class extends Component {
             <hr class="border-white/20 my-8">
 
             <p class="text-lg mb-8 max-w-[48ch]">
-                Las inscripciones para la recepción de regalos navideños para la comuna de Pichilemu está disponible en el siguiente formulario.
+                Las inscripciones para la recepción de regalos navideños para la comuna de Pichilemu está disponible en
+                el siguiente formulario.
             </p>
 
             <div class="mt-6">
-                <a href="{{ route('inscribir') }}" class="bg-emerald-600/40 text-white py-3 px-6 rounded-lg hover:bg-emerald-600/80 transition inline-flex items-center space-x-2">
-                    <x-fas-gift class="h-6" />
+                <a href="{{ route('inscribir') }}"
+                   class="bg-emerald-600/40 text-white py-3 px-6 rounded-lg hover:bg-emerald-600/80 transition inline-flex items-center space-x-2">
+                    <x-fas-gift class="h-6"/>
                     <span class="font-medium">Inscribir</span>
                 </a>
             </div>
